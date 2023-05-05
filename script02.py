99e421db882d38ff65ac1bba2c69e20d6f147180
Jon Minton <jon.will.minton@gmail.com>
2023-05-05 16:30:35 +0100

    add second working example (rpy2)

--- script02.py
+++ script02.py
@@ -1,7 +1,7 @@
-# First example from following website
+# second example from following website
 # https://appsilon.com/use-r-and-python-together/#:~:text=You%20can%20import%20any%20Python,and%20functions%20declared%20with%20R.&text=And%20that's%20how%20you%20can%20run,in%20R%20and%20R%20Markdown.
 
-import subprocess
+from rpy2 import robjects
 
-res = subprocess.call("Rscript /Users/JonMinton/repos/python-r-101/r/script.r", shell = True)
-res
+pi = robjects.r['pi']
+print(pi)
